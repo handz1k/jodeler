22851794a2d371394a7a6d153f322652497ec98f
hhddzz <markait22@gmail.com>
2024-03-02 15:01:09 +0200

    added toast notifications for login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,32 +1,40 @@
 import "../index.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import loginService from "../services/login";
 import jodelService from "../services/jodel";
 import Notification from "./Notification";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const LoginForm = ({ onLoginSuccess }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [notification, setNotification] = useState("");
+  const toastId = useRef(null);
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
+      toastId.current = toast.loading("Loading...");
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       jodelService.setToken(user.token);
       onLoginSuccess(user);
     } catch (expection) {
-      setNotification("invalid username or password");
-      setTimeout(() => {
-        setNotification("");
-      }, 2000);
+      toast.update(toastId.current, {
+        render: "Invalid username or password",
+        autoClose: 2000,
+        type: "error",
+        isLoading: false,
+        position: "bottom-left",
+      });
     }
   };
 
   return (
     <div className="login-form">
       <Notification message={notification} />
+      <ToastContainer />
       <form onSubmit={handleLogin}>
         <h2>
           Login to <i>Jodeler ✨</i>
